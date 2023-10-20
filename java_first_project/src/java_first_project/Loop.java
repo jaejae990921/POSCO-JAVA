23fe96ffa52ed346659f3cdc2bb99d93dea59baa
jaejae990921 <jaejae990921@naver.com>
2023-10-20 17:46:39 +0900

    study : 반복문

---
+++ java_first_project/src/java_first_project/Loop.java
@@ -0,0 +1,16 @@
+package java_first_project;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.print(i);
+		}
+
+		int i = 0;
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
